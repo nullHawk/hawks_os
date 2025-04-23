95c63711dccfdf9646295e0c7e14973ac8ceac2e
Suryansh <suryanshsinghshakya1@gmail.com>
2025-04-23 05:31:10 +0000

    update: terminal color

--- main.c
+++ main.c
@@ -20,15 +20,16 @@ int _start(){
     CELL = 0;
 
     cls();
-    setMonitorColor(0xa5);
+    setMonitorColor(0x1B);
 
     char Welcome[] = "Welcome to HawksOS : Copyright 2025\n";
     char Tagline[] = "Command Line Version 0.1\n\n";
     char Prompt[] = "HawksOS> ";
+    char paawan[] = "I am learning OS development\n";
 
     printString(Welcome);
     printString(Tagline);
-    printColorString(Prompt, 0xa8);
+    printColorString(Prompt, 0x1F);
 }
 
 void cls(){
