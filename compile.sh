67df12be106ca4acf80881126adefc7619469db8
Suryansh <suryanshsinghshakya1@gmail.com>
2025-04-23 07:45:00 +0000

    add: keboard driver

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 nasm boot.asm -f bin -o bin/bootsect.bin
 nasm kernel_entry.asm -f elf -o bin/entry.bin
+nasm IDT.asm -f elf -o bin/IDT.bin
 
 gcc -m32 -ffreestanding -c main.c -o bin/kernel.o
 
-ld -m elf_i386 -Ttext 0x1000 -o bin/kernel.img bin/entry.bin bin/kernel.o  # Added -m elf_i386
+ld -m elf_i386 -Ttext 0x1000 -o bin/kernel.img bin/entry.bin bin/kernel.o bin/IDT.bin  # Added -m elf_i386
 objcopy -O binary -j .text bin/kernel.img bin/kernel.bin
 cat bin/bootsect.bin bin/kernel.bin > bin/os-image
 
--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+#define PIC1_C 0x20 // Command port for the master PIC
+#define PIC1_D 0x21 // Data port for the master PIC
+#define PIC2_C 0xa0 // Command port for the slave PIC
+#define PIC2_D 0xa1 // Data Port for the slave PIC
+
+#define ICW1_DEF 0x10
+#define ICW1_ICW4 0x01
+#define ICW4_x86 0x01
+
 void cls();
 void setMonitorColor(char);
 
@@ -11,13 +20,36 @@ void printColorChar(char, char);
 
 void getDecAscii(int);
 
+void initIDT();
+extern void _loadIdt();
+extern void _isr1_Handler();
+void handleKeypress(int);
+void pressed(char);
+void picRemap();
+
+unsigned char inportb(unsigned short);
+void outportb(unsigned short, unsigned char);
+
 char* TM_START;
 char NumberAscii[10];
 int CELL;
 
+struct IDT_ENTRY{
+	unsigned short base_Lower;
+	unsigned short selector;
+	unsigned char zero;
+	unsigned char flags;
+	unsigned short base_Higher;
+};
+
+struct IDT_ENTRY _idt[256];
+extern unsigned int _isr1;
+unsigned int base;
+
 int _start(){
     TM_START = (char*) 0xb8000;
     CELL = 0;
+	base = (unsigned int)&_isr1;
 
     cls();
     setMonitorColor(0x1F);
@@ -29,6 +61,8 @@ int _start(){
     printColorString(Welcome, 0x1B);
     printColorString(Tagline, 0x1B);
     printColorString(Prompt, 0x1E);
+
+	initIDT();
 }
 
 void cls(){
@@ -127,4 +161,93 @@ void getDecAscii(int num){
         j++;
     }
     NumberAscii[j] = 0;
+}
+
+void initIDT(){
+	// Set the lower 16 bits of the ISR1 handler address in the IDT entry
+	_idt[1].base_Lower = (base & 0xFFFF);
+
+	// Set the higher 16 bits of the ISR1 handler address in the IDT entry
+	_idt[1].base_Higher = (base >> 16) & 0xFFFF;
+
+	// Set the code segment selector for this IDT entry (0x08 is the kernel code segment)
+	_idt[1].selector = 0x08;
+
+	// Reserved field, must be zero
+	_idt[1].zero = 0;
+
+	// Set the flags for this IDT entry (0x8e = present, privilege level 0, 32-bit interrupt gate)
+	_idt[1].flags = 0x8e;
+
+	// Remap the PIC (Programmable Interrupt Controller) to avoid conflicts with CPU exceptions
+	picRemap();
+
+	// Enable IRQ1 (keyboard interrupt) by unmasking it in the PIC
+	outportb(0x21, 0xfd); // Mask all IRQs except IRQ1 on PIC1
+	outportb(0xa1, 0xff); // Mask all IRQs on PIC2
+
+	// Load the IDT (Interrupt Descriptor Table) into the CPU
+	_loadIdt();
+}
+
+unsigned char inportb(unsigned short _port){
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+void outportb(unsigned short _port, unsigned char _data){
+    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+extern void _isr1_Handler(){
+	handleKeypress(inportb(0x60));
+	outportb(0x20 , 0x20);
+	outportb(0xa0 , 0x20);
+}
+
+void handleKeypress(int code){
+	char Scancode[] = {
+		0 , 0 , '1' , '2' ,
+		'3' , '4' , '5' , '6' , 
+		'7' , '8' , '9' , '0' , 
+		'-' , '=' , 0 , 0 , 'Q' , 
+		'W' , 'E' , 'R' , 'T' , 'Y' ,
+		'U' , 'I' , 'O' , 'P' , '[' , ']' , 
+		0 , 0 , 'A' , 'S' , 'D' , 'F' , 'G' , 
+		'H' , 'J' , 'K' , 'L' , ';' , '\'' , '`' , 
+		0 , '\\' , 'Z' , 'X' , 'C' , 'V' , 'B' , 'N' , 'M' ,
+		',' , '.' , '/' , 0 , '*' , 0 , ' '
+	};
+	
+	if(code == 0x1c)
+		printChar('\n');
+	else if(code < 0x3a)
+		pressed(Scancode[code]);
+}
+
+void pressed(char key){
+	printChar(key);
+}
+
+// Remaps IRQ so that it doesn't overlap with CPU executions
+void picRemap(){
+	unsigned char a , b;
+	a = inportb(PIC1_D);
+	b = inportb(PIC2_D);
+
+	outportb(PIC1_C , ICW1_DEF | ICW1_ICW4);
+	outportb(PIC2_C , ICW1_DEF | ICW1_ICW4);
+
+	outportb(PIC1_D , 0);
+	outportb(PIC2_D , 8);
+
+	outportb(PIC1_D , 4);
+	outportb(PIC2_D , 2);
+
+	outportb(PIC1_D , ICW4_x86);
+	outportb(PIC2_D , ICW4_x86);
+
+	outportb(PIC1_D , a);
+	outportb(PIC2_D , b);
 }
